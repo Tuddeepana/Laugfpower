5068157ef15862cf4e5cf6e4f5672e9e00e518d7
Tuddeepana <tauddeepana@gmail.com>
2024-10-14 12:02:00 +0530

    color added

--- src/components/card/BreadCrum.jsx
+++ src/components/card/BreadCrum.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function BreadCrum({ text }) {
   return (
-    <div 
+    <div
       className="w-full p-4"
       style={{
         // background: 'linear-gradient(45deg, #389b49, #7fb948)',
       }}
     >
       <div className="container mx-auto">
-        <h1 className="text-4xl font-bold text-left text-green-600	">{text}</h1>
+        <h1 className="text-4xl font-bold text-center" style={{color: '#2e9b47'}}>{text}</h1>
       </div>
     </div>
   );
